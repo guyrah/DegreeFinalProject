713735ab5ef79cbf08e4df018302870e0b0d1574
Sunny <bsunnyp@gmail.com>
2017-05-24 00:01:52 +0200

    making sure all the code in the git

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class logLevels:
     warn = 1
     info = 2
     debug = 3
-    level = info
+    level = debug
 
 class config:
     mongoDbURL = 'mongodb://193.106.55.77:27017'
     dataSetFilePath = "tagged_data.json"
     VOCABULARY_PATH = 'vocabulary.txt'
     dataFieldName = 'text'
-    testSetSize = 10
+    testSetSize = 100
     refreshTaggedData = False
